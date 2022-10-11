b3f71aa16af575e3d73c8db13459085be31af17b
Harry Shamansky <harry.shamansky@gmail.com>
2022-10-10 22:18:18 -0700

    Update Package.swift for watchOS

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v13),
         .tvOS(.v13),
+        .watchOS(.v9)
     ],
     products: [
         .library(name: "CloudStorage", targets: ["CloudStorage"]),
